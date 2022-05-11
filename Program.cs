46714a1ee5d7c809518c2ae7c862aff6046aed63
syrvyss <lybkern@gmail.com>
2022-05-11 22:29:14 +0200

    Added CalculateMaxIlt and cached KonditalResult

--- Program.cs
+++ Program.cs
@@ -10,13 +10,19 @@
             Console.Write("Maxpuls: ");
             double Maxpuls = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("\nKondital (ml/kg/min): {0}", Kondital.CalculateKondital(Hvilepuls, Maxpuls));
+            double KonditalResult = Kondital.CalculateKondital(Hvilepuls, Maxpuls);
+            Console.WriteLine("\nKondital (ml/kg/min): {0}", KonditalResult);
+            Console.WriteLine("Maxiltoptagelse (l/ml): {0}", Kondital.CalculateMaxIlt(KonditalResult, Weight));
         }
     }
 
     class Kondital {
         public static double CalculateKondital(double Hvilepuls, double Maxpuls) {
             return Math.Round((Maxpuls / Hvilepuls) * 15.3, 0);
+        }
+
+        public static double CalculateMaxIlt(double Kondital, double Weight) {
+            return Math.Round((Kondital * Weight) / 1000);
         }
     }
 }
